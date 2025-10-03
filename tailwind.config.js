1649b26ee260c912645f0a1015fc09b89d8bbb9b
Loli <loladigitalizada@gmail.com>
2025-10-03 18:48:12 +0200

    Se actualizan archivos
    - angular.json
    - import en app.css
    - se añade _redirects (para que si hay un error en una página, lleve al inicio)
    - creación de tailwind.config.json

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  // CRÍTICO: Indica a Tailwind que analice todos los archivos HTML y TS 
+  // en la carpeta src/ para encontrar las clases utilizadas.
+  content: [
+    "./src/**/*.{html,ts}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
