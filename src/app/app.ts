e9f04d24f77ff3fffadc0fc1682842cbdf398eb2
Loli <loladigitalizada@gmail.com>
2025-10-03 18:21:48 +0200

    Se añaden los archivos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,15 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router'; // <--- ¡Importación necesaria!
+import { routes } from './app.routes'; // <--- ¡Importación necesaria!
+
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes) 
+  ]
+};
 
 @Component({
   selector: 'app-root',
@@ -8,5 +18,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('flash-cards-game');
+  protected readonly title = signal('quiz-app');
 }
