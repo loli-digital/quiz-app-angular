977da2f34c920d5baf5cccedfc5158614ec81394
Loli <loladigitalizada@gmail.com>
2025-10-06 14:43:09 +0200

    Modifica tailwind.config.ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}", 
     "./src/*.html", 
+    "./src/app/**/*.{html,ts,css,scss}", 
+    "./src/index.html"
   ],
   theme: {
     extend: {},
