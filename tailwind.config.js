00794f2a82aac71cd13b28abe45c32c74bf1d217
Loli <loladigitalizada@gmail.com>
2025-10-06 14:03:32 +0200

    fix: Final CSS and Git configuration; added .gitattributes to fix line ending warning.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  // CRÍTICO: Indica a Tailwind que analice todos los archivos HTML y TS 
-  // en la carpeta src/ para encontrar las clases utilizadas.
+
   content: [
-    "./src/**/*.{html,ts}", 
-    "./src/app/app.html" 
+    "./src/**/*.{html,ts,js}", 
+    "./src/index.html", 
   ],
   theme: {
-    extend: {},
+  
   },
   plugins: [],
 }
