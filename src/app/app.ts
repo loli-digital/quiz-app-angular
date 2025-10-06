aca15e976632029850de85ea0ec5921655922a1e
Loli <loladigitalizada@gmail.com>
2025-10-06 14:35:53 +0200

    Change config tailwind

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router'; // <--- ¡Importación necesaria!
-import { routes } from './app.routes'; // <--- ¡Importación necesaria!
-
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 import { Component, signal } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,9 +15,11 @@ export const appConfig: ApplicationConfig = {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
+  standalone: true,
+  providers: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('quiz-app');
